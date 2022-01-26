ac4c6bb2e24b98c18bcaba774e9d2016fcd6337d
Chi-O <chioma.o3762@gmail.com>
2022-01-26 14:33:07 -0500

    added in-class demo code

---
+++ week 2 - classes and objects/demo.cpp
@@ -0,0 +1,15 @@
+// in-class Class demonstration
+// 26/01/2022
+
+#include <iostream>
+
+using namespace std;
+
+class Vehicle {
+
+}; // Classes end with a semicolon
+
+int main()
+{
+    return 0;
+}
