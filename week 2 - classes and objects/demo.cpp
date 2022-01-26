2b74f4c9f887375c687daaa302423f8cae5cf42a
Chi-O <chioma.o3762@gmail.com>
2022-01-26 14:50:43 -0500

    declared class demonstrated methods

--- week 2 - classes and objects/demo.cpp
+++ week 2 - classes and objects/demo.cpp
@@ -5,11 +5,42 @@
 
 using namespace std;
 
-class Vehicle {
+class Vehicle
+{
+    public: // access modifiers
+
+    // properties .i.e. variables
+    string name;
+    string type;
+    string color;
+    int kms;
+
+    // actions .i.e. methods
+    void carDetails()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Type: " << type << endl;
+        cout << "Color: " << color << endl;
+        cout << "KMs: " << kms << endl;
+    }
+    void drive()
+    {
+        cout << "I can drive" << endl;
+    }
 
 }; // Classes end with a semicolon
 
 int main()
 {
+    // create an object of a class
+    Vehicle car1;
+    car1.kms = 50000;
+    car1.name = "Honda Civic";
+    car1.type = "Compact Car";
+    car1.color = "Space Grey";
+
+    car1.drive();
+    car1.carDetails();
+
     return 0;
 }
