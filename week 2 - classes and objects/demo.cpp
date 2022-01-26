9ad761d9b5e7ca75bf7879f4da6ead4921ba5642
Chi-O <chioma.o3762@gmail.com>
2022-01-26 15:34:47 -0500

    checkpoint: setters and getters

--- week 2 - classes and objects/demo.cpp
+++ week 2 - classes and objects/demo.cpp
@@ -7,13 +7,44 @@ using namespace std;
 
 class Vehicle
 {
-    public: // access modifiers
-
+private: // access modifiers
     // properties .i.e. variables
     string name;
     string type;
     string color;
     int kms;
+
+public:
+    //constructor
+    Vehicle(string n, string t, string c, int k)
+    {
+        name = n;
+        type = t;
+        color = c;
+        kms = k;
+    }
+
+    Vehicle()
+    {
+        name = "Audi";
+        type = "Cool";
+        color = "Pink";
+        kms = 702312;
+    }
+
+    // getter and setters
+    void getKMS()
+    {
+        cout << kms << endl;
+    }
+
+    void setKMS(int k)
+    {
+        if (k >= 0)
+        {
+            kms = k;
+        }
+    }
 
     // actions .i.e. methods
     void carDetails()
@@ -22,10 +53,13 @@ class Vehicle
         cout << "Type: " << type << endl;
         cout << "Color: " << color << endl;
         cout << "KMs: " << kms << endl;
+        cout << endl;
     }
+
     void drive()
     {
-        cout << "I can drive" << endl;
+        cout << "I can drive 🚗💨 \n"
+             << endl;
     }
 
 }; // Classes end with a semicolon
@@ -33,14 +67,16 @@ class Vehicle
 int main()
 {
     // create an object of a class
-    Vehicle car1;
-    car1.kms = 50000;
-    car1.name = "Honda Civic";
-    car1.type = "Compact Car";
-    car1.color = "Space Grey";
-
+    Vehicle car1 = Vehicle("Honda CRV", "Compact", "Orange", 39843);
     car1.drive();
-    car1.carDetails();
+    // car1.carDetails();
+    car1.getKMS();
+    car1.setKMS(56789);
+    car1.getKMS();
+
+
+    // Vehicle car2 = Vehicle();
+    // car2.carDetails();
 
     return 0;
 }
