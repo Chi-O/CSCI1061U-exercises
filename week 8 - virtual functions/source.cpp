8f45732bb7c02a4599dd112b60f24d3669769803
Chi-O <chioma.o3762@gmail.com>
2022-03-09 14:52:10 -0500

    the problem of default parent constructor

--- week 8 - virtual functions/source.cpp
+++ week 8 - virtual functions/source.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 class Vehicle
 {
-private: // access modifiers
-    // properties .i.e. variables
-    int* sensors;
+protected: // available to child classes only
+    string name;
+    int kms;
 
 public:
-    //constructor
-    Vehicle()
+    // constructor
+    Vehicle(string name, int kms)
     {
-        sensors = new int[25]; // create an array of 25 integers in the heap memory
-        cout << "default constructor \n";
+        this->name = name;
+        this->kms = kms;
+        cout << "parameterized Vehicle constructor \n";
     }
-
     void drive()
     {
         cout << "I can drive 🚗💨 \n";
@@ -23,19 +23,36 @@ public:
 
     ~Vehicle()
     {
-        // you can deallocate memory here in the destructor
-        delete[] sensors;
-        // avoid dangling pointer
-        sensors = nullptr;
         cout << "default destructor \n";
     }
 
 }; // Classes end with a semicolon
 
+class Car : public Vehicle
+{
+public:
+    // constructor
+    Car(string name, int kms)
+    { // by default, child class automatically calls default constructor of parent class
+        this->name = name;
+        this->kms = kms;
+        cout << "parameterized Car constructor \n";
+    }
+
+    void goToPicnic()
+    {
+        cout << "going to picnic \n";
+    }
+
+    ~Car()
+    {
+        cout << "Car default destructor \n";
+    }
+};
+
 int main()
 {
-    Vehicle vehicle;
-    vehicle.drive();
-    // destructor is automatically called whenever an object is destroyed
+    Car car = Car("Toyota", 123);
+
     return 0;
 }
