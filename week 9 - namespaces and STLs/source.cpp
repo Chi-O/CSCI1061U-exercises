bd0df0686c44323e8484f797f82d815e3aa9343f
Chi-O <chioma.o3762@gmail.com>
2022-03-16 15:08:14 -0400

    class example

--- week 9 - namespaces and STLs/source.cpp
+++ week 9 - namespaces and STLs/source.cpp
@@ -1,14 +1,60 @@
-#include "vehicle.h"
-#include "anotherVehicle.h"
+#include <iostream>
+
+using namespace std;
+
+class X
+{
+private:
+    int x;
+
+public:
+    // default constructor
+    X()
+    {
+        x = 0;
+    }
+    X(int x)
+    {
+        this->x = x;
+    }
+    int getX()
+    {
+        return x;
+    }
+};
+
+class Y
+{
+private:
+    int y;
+
+public:
+    // default constructor
+    Y()
+    {
+        y = 0;
+    }
+    Y(int y)
+    {
+        this->y = y;
+    }
+    int getY()
+    {
+        return y;
+    }
+};
+
+int sum(X x1, Y y1)
+{
+    return x1.getX() + y1.getY();
+}
 
 int main()
 {
-    galactic::Vehicle vehicle; // access classes
-    vehicle.drive();
-    galactic::globFunc(); // access global functions as well
+    X x1 = X(5); 
+    Y y1 = Y(7);
 
-    milkyWay::Vehicle v2;
-    v2.startEngine();
+    cout << "Sum: " << sum(x1, y1) << "\n"; // works with int only
 
     return 0;
 }
