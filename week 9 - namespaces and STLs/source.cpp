33af1f72f798e0f25f194be59c42fd01e0a7920b
Chi-O <chioma.o3762@gmail.com>
2022-03-16 20:52:33 -0400

    finished in-class demo

--- week 9 - namespaces and STLs/source.cpp
+++ week 9 - namespaces and STLs/source.cpp
@@ -1,54 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <array>
 
 using namespace std;
 
-template <class T>
-class Stack 
-{
-private:
-    vector<T> stack;
-public:
-    void push(T e)
-    {
-        stack.push_back(e);
-    }
-
-    void pop()
-    {
-        stack.pop_back();
-    }
-
-    // FILO first in, last out
-    void display()
-    {
-        cout << "from top to bottom... \n";
-        for (int i = stack.size() - 1; i >= 0; i--)
-        {
-            cout << "element [" << i << "] = " << stack[i] << "\n";
-        }
-    }
-};
-
 int main()
-{   
-    stack<double> s;
+{
+    array<int, 5> arr;
     double num;
 
     for (int i = 0; i < 5; i++)
     {
         cout << "enter element [" << i << "] = ";
         cin >> num;
-        s.push(num);
+        arr[i] = num;
     }
 
-    while (!s.empty())
+    for (int i = 0; i < 5; i++)
     {
-        cout << s.top() << "\n";
-        s.pop();
+        cout << "element [" << i << "] = " << arr[i] << "\n";
     }
-    
+
 
     return 0;
 }
