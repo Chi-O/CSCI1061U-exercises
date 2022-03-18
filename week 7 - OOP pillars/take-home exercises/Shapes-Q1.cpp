0ef43a8e1bfa7623bf01da9fcfef83d2fce0f21d
Chi-O <chioma.o3762@gmail.com>
2022-03-18 10:08:01 -0400

    finished exercise level 2

--- week 7 - OOP pillars/take-home exercises/Shapes-Q1.cpp
+++ week 7 - OOP pillars/take-home exercises/Shapes-Q1.cpp
@@ -9,14 +9,21 @@ protected:
     int height;
 
 public:
-    void setHeight(int height)
-    {
-        this->height = height;
-    }
-    void setWidth(int width)
+    // corresponding parameterized constructors in the parent class Shape
+    Shape(int width)
     {
         this->width = width;
+
+        cout << "Shape constructor \n";
     }
+    Shape(int height, int width)
+    {
+        this->height = height;
+        this->width = width;
+
+        cout << "Shape constructor \n";
+    }
+
     void calculateArea()
     {
         cout << width * height << "\n";
@@ -26,6 +33,12 @@ public:
 class Square : public Shape
 {
 public:
+    Square(int width) : Shape(width)
+    {
+        this->width = width;
+
+        cout << "Square constructor \n";
+    }
     void calculateArea()
     {
         cout << "Area of square: " << height * height << "\n";
@@ -35,21 +48,25 @@ public:
 class Rectangle : public Shape
 {
 public:
+    Rectangle(int height, int width) : Shape(height, width)
+    {
+        this->height = height;
+        this->width = width;
+
+        cout << "Rectangle constructor \n";
+    }
     void calculateArea()
     {
-        cout << "Area of rectangle: "<< width * height << "\n";
+        cout << "Area of rectangle: " << width * height << "\n";
     }
 };
 
 int main()
 {
-    Square s;
-    s.setHeight(5);
+    Square s = Square(5);
     s.calculateArea();
 
-    Rectangle r;
-    r.setHeight(50);
-    r.setWidth(15);
+    Rectangle r = Rectangle(5, 60);
     r.calculateArea();
 
     return 0;
