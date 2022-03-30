9bac9061feb93711668c5d6d7a5817d0a387c15d
Chi-O <chioma.o3762@gmail.com>
2022-03-30 15:01:32 -0400

    in-class recursion example power

--- week 10 - function templates/source.cpp
+++ week 10 - function templates/source.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 int main()
 {
+    double i = 5;
+    cout << i << "\n";
+
+    cout << typeid(typeid(i).name()).name() << "\n";
+
+    // lambda function syntax
+    // [capture list](parameter list) {function};
+    []()
+    { cout << "Hello World"; };
+
     return 0;
 }
